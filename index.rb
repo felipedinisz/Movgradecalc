af5a06858cb65e1946eb329319ec191403d7903e
Felipe Diniz <100390391+oArianoo@users.noreply.github.com>
2022-08-19 10:24:54 -0300

    Create index.rb

--- index.rb
+++ index.rb
@@ -1,42 +1,42 @@
 loop do
-puts "\n Deseja saber quanto falta para passar de ano em alguma matéria? \n 1 - Sim \n 0 - Não"
+puts "\n Deseja saber quanto falta para passar de ano em alguma matéria? \n Sim - 0 \n Não - 1" 
 print "Opção: "
 option = gets.chomp.to_i
 
 case option
-
-when 1
-
-print "\n Insira a matéria: "
+when 0
+print "\n Insira uma matéria: "
 subject = gets.chomp
 
 grades = []
 i = 1
 
-4.times do 
- print "\n Coloque a nota do #{i} Bimestre: "
- grades.push gets.chomp.to_f
- i += 1
+4.times do
+ print "\n Insira sua nota do #{i}° Bimestre: "
+  grades.push gets.chomp.to_f
+  i += 1
 end
 
-value = 0
-  
+  value = 0
+
 grades.each do |grade|
-   value += grade
+  value += grade
 end
 
 result = 60 - value
 
-if value > 60
- puts "\n Você já passou de ano em #{subject}"
-else
- puts "\n Faltam #{result} pontos para você passar de ano em #{subject}"
-end
-
-when 0
- Break
-
+if value >= 60
+  puts "Você já passou em #{subject}! Você tem #{value} pontos."
 else 
- Puts "\n Selecione uma opção válida."
+  puts "Faltam ainda #{result} pontos para passar em #{subject}."
 end
 
+
+
+when 1
+  break
+else
+ puts "\n Coloque uma opção válida."
+end
+
+end
